a757eff6bf6e2167e2bdb1161f2899f3e6d13da8
Bruce Ediger <bediger8@gmail.com>
2021-02-03 09:40:59 -0700

    Better comment

--- compare.go
+++ compare.go
@@ -9,8 +9,7 @@ import (
 )
 
 /*
- * Given an integer n, find the next biggest integer with the same number of
- * 1-bits on.  For example, given the number 6 (0110 in binary), return 9 (1001).
+ * Compare the 2 algorithms with an input from the command line.
  */
 
 func main() {
